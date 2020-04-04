a5bf8263c14bc50e02fb0cc2fca79235ac22893f
lecaptain-sudo <raphael.cosperec@live.fr>
2020-04-04 12:53:25 +0200

    Ajout de constantes Delete Insert Modify

--- src/main/java/utils/Constantes.java
+++ src/main/java/utils/Constantes.java
@@ -6,4 +6,7 @@ public class Constantes {
     public final static String PROGBYNAME = "SELECT * from PROGRAMMEUR where NOM like ?";
     public final static String PROGBYFIRSTNAME = "SELECT * from PROGRAMMEUR where PRENOM like ?";
     public final static String PROGBYYEAR = "SELECT * from PROGRAMMEUR where ANNAISSANCE like ?";
+    public final static String DELPROG = "DELETE FROM PROGRAMMEUR WHERE ID=?";
+    public final static String CHANGESALARY = "UPDATE PROGRAMMEUR SET SALAIRE=? WHERE ID=?";
+    public final static String CREATEPROG = "INSERT INTO PROGRAMMEUR(NOM,PRENOM,ADRESSE,PSEUDO,RESPONSABLE,HOBBY,ANNAISSANCE,SALAIRE,PRIME) VALUES(?,?,?,?,?,?,?,?,?)";
 }
